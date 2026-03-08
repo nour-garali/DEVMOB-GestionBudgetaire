8d02ccac1e40b136fe4c4a5eab3b88156a351e54
Nour Garali <nourgarali12345@gmail.com>
2026-03-08 10:00:00 +0100

    feat: Configurer l'entrée principale de l'application et les options Firebase

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'providers/auth_provider.dart';
+import 'providers/transaction_provider.dart';
+import 'providers/BudgetProvider.dart';
+import 'views/splash_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await initializeDateFormatting('fr_FR', null);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => TransactionProvider()),
+        ChangeNotifierProvider(create: (_) => BudgetGoalProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DEVMOB - Gestion Budgétaire',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1644FF),
+          background: const Color(0xFFF8F9FF),
+        ),
+        textTheme: Theme.of(context).textTheme,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
